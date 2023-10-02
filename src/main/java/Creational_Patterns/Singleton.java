41ac4ef25b768fbd415e2c2d7a7da203f0adc205
Nikhil-197 <mahajannikhil177@gmail.com>
2023-10-02 18:52:46 +0530

    Creational Design Pattern

---
+++ src/main/java/Creational_Patterns/Singleton.java
@@ -0,0 +1,35 @@
+package Creational_Patterns;
+
+public class Singleton {
+
+}
+
+class SingletonImplement {
+
+    private static SingletonImplement instance;
+
+    private SingletonImplement()
+    {
+        
+    }
+    public static SingletonImplement getInstance()
+    {
+        if(instance==null)
+            instance=new SingletonImplement();
+            return instance;
+    }
+}
+
+/*
+The Singleton design pattern is a creational pattern that ensures a class has only one instance and provides a global point of access to that instance. Here are some important points to consider when working with the Singleton pattern:
+
+Private Constructor: The Singleton class has a private constructor to prevent direct instantiation of the class from outside. This ensures that no more than one instance can be created.
+
+Static Instance: It typically maintains a private static instance variable that holds the single instance of the class.
+
+Lazy Initialization: The Singleton instance is often created lazily, meaning it's created the first time it's requested, not during class loading. This helps save resources if the instance is never used.
+
+Global Access Point: A public static method (often named getInstance()) is provided to access the Singleton instance. This method is responsible for creating the instance if it doesn't exist or returning the existing instance.
+
+
+*/
